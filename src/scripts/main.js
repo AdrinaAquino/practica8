b0c68597322000a5056a880bfd6d780563595b56
adrianasheila <adrisheiaquiblan@gmail.com>
2025-03-19 01:53:55 -0400

    calculadora con restriccion de operadores en pantalla blanca

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,33 +1,28 @@
 const pantalla = document.querySelector("#pantalla")
-const botones = document.querySelector("#botones")
+const botones = document.querySelectorAll("button")
+const numeros = document.querySelectorAll("#num")
+const operadores = document.querySelectorAll("#operador")
+const borrar = document.querySelectorAll("#borrar")
+const igual = document.querySelector("#igual")
 
-const teclas = [7, 8, 9, "AC", 4, 5, 6, "X", 1, 2, 3, "/", "-", 0, "+", "*", "="]
-
-
-
-
-function enviar(lista, indexhtml) {
-    indexhtml.innerHTML = ""
-    lista.forEach((a) => {
-        const template = `<button class="tecla" id="btn">${a}</button>`
-        indexhtml.innerHTML += template
-    })
-}
-enviar(teclas, botones)
-
-
-teclas.forEach((btn) => {
-    btn.addEventListener('click', () => {
-        if (btn === "=") {
-            pantalla.value = eval(pantalla.value);
-        } else if (btn === "AC") {
+botones.forEach(btn => {
+    btn.addEventListener("click", () => {
+        if (btn.textContent === "AC") {
             pantalla.value = "";
-        } else if (btn === "X") {
-            pantalla.value = pantalla.value.slice(0, -1);
-        } else {
-            pantalla.value += btn
+        } else if (btn.textContent === "X") {
+            pantalla.value = pantalla.value.slice(0, -1)
+        } else if (btn.textContent === "=") {
+            pantalla.value = eval(pantalla.value)
+        } else if (btn.textContent === "+" && pantalla.value === "") {
+            pantalla.value = "";
+        } else if (btn.textContent === "-" && pantalla.value === "") {
+            pantalla.value = "";
+        } else if (btn.textContent === "*" && pantalla.value === "") {
+            pantalla.value = "";
+        } else if (btn.textContent === "/" && pantalla.value === "") {
+            pantalla.value = "";
+        }else {
+            pantalla.value += btn.textContent
         }
     })
 })
-
-
